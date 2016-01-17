4f4df2af262843f7b4fabe8e820c1d3c7b5d3390
Teck <ch0pst1x88@gmail.com>
2016-01-16 22:48:39 -0800

    first survey form in mean

---
+++ server.js
@@ -0,0 +1,40 @@
+// require express
+
+var express = require("express");
+
+// path module -- try to figure out where and why we use this
+
+var path = require("path");
+
+// create the express app
+
+var app = express();
+
+var bodyParser = require('body-parser');
+// use it!
+app.use(bodyParser.urlencoded());
+
+app.use(express.static(path.join(__dirname, "./static")));
+
+// setting up ejs and our views folder
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+// root route to render the index.ejs view
+app.get('/', function(req, res) {
+ res.render("index");
+})
+
+// post route for adding a user
+app.post('/users', function(req, res) {
+ var info = ("POST DATA", req.body);
+ console.log(info);
+ // This is where we would add the user to the database
+ // Then redirect to the root route
+ res.render('stuff', {stuff: info});
+})
+
+// tell the express app to listen on port 8000
+app.listen(8000, function() {
+ console.log("listening on port 8000");
+})
